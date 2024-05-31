2199a4388a3d90d7c3f9827e57581a78db699454
Kevin Wang <kwangsan@gmail.com>
2024-05-31 11:18:30 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,10 @@ export class MyDurableObject extends DurableObject {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		// There are two errors here:
-
-		// 1. TypeError: Durable Object ID is not valid for this namespace.
-		// ----------------------------------------
 		let idString = new URL(request.url).pathname.replace(/^\//, '');
 		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromString(idString); // This line throws an error.
 		let stub = env.MY_DURABLE_OBJECT.get(id);
 		let greeting = await stub.sayHello('world');
 		return new Response(greeting);
-
-		// 2. TypeError: The receiving Durable Object does not support RPC,
-		// because its class was not declared with `extends DurableObject`.
-		// In order to enable RPC, make sure your class extends the special class
-		// `DurableObject`, which can be imported from the module "cloudflare:workers".
-		// ----------------------------------------
-		// let id = env.MY_DURABLE_OBJECT.newUniqueId();
-		// let stub = env.MY_DURABLE_OBJECT.get(id);
-		// let greeting = await stub.sayHello('world'); // This line throws an error.
-		// return new Response(greeting);
 	},
 };
